e2d14ef2b88222d683feb62ec3cb3488d4f2d737
Wilis Cavalcante <56607831+wiliscavalcante@users.noreply.github.com>
2023-07-13 01:27:48 -0300

    Update add-volume-ec2.sh

--- add-volume-ec2.sh
+++ add-volume-ec2.sh
@@ -3,6 +3,10 @@
 # Atualiza os pacotes do sistema
 sudo yum update -y
 
+# Faz backup do conteúdo da home do usuário ec2-user para /tmp
+  sudo tar -czf "/tmp/ec2-user_backup.tar.gz" -C /home/ec2-user .
+
+
 # Configuração dos dispositivos e pontos de montagem
 DEVICES=("/dev/nvme1n1" "/dev/nvme1n2")
 MOUNT_POINTS=("/data" "/backup")
@@ -12,8 +16,8 @@ for ((i=0; i<${#DEVICES[@]}; i++)); do
   DEVICE=${DEVICES[$i]}
   MOUNT_POINT=${MOUNT_POINTS[$i]}
 
-  # Formata o dispositivo como XFS, se necessário
-  if ! sudo file -s "$DEVICE" | grep -q "XFS filesystem"; then
+   # Verifica se o dispositivo já está formatado como XFS
+  if ! sudo xfs_info "$DEVICE" >/dev/null 2>&1; then
     sudo mkfs.xfs "$DEVICE"
   fi
 
@@ -36,3 +40,9 @@ for ((i=0; i<${#DEVICES[@]}; i++)); do
   # Define a permissão correta para o usuário ec2-user no ponto de montagem
   sudo chown -R ec2-user:ec2-user "$MOUNT_POINT"
 done
+
+# Restaura o conteúdo do backup na nova home do usuário ec2-user
+sudo tar -xzf "/tmp/ec2-user_backup.tar.gz" -C /home/ec2-user
+
+# Remove o arquivo de backup
+sudo rm "/tmp/ec2-user_backup.tar.gz"
