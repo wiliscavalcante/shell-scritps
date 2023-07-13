4a056c53d356905806da74c3abe0c2dbad295901
Wilis Cavalcante <56607831+wiliscavalcante@users.noreply.github.com>
2023-07-12 23:46:26 -0300

    Create add-volume-ec2.sh

---
+++ add-volume-ec2.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Configuração dos dispositivos e pontos de montagem
+DEVICES=("/dev/nvme1n1" "/dev/nvme1n2")
+MOUNT_POINTS=("/data" "/backup")
+
+# Loop para formatar e montar os dispositivos
+for ((i=0; i<${#DEVICES[@]}; i++)); do
+  DEVICE=${DEVICES[$i]}
+  MOUNT_POINT=${MOUNT_POINTS[$i]}
+
+  # Verifica se o dispositivo já está formatado como XFS
+  if ! sudo xfs_info "$DEVICE" >/dev/null 2>&1; then
+    sudo mkfs.xfs "$DEVICE"
+  fi
+
+  # Cria o diretório de ponto de montagem, se não existir
+  if [ ! -d "$MOUNT_POINT" ]; then
+    sudo mkdir "$MOUNT_POINT"
+  fi
+
+  # Monta o dispositivo no ponto de montagem
+  sudo mount "$DEVICE" "$MOUNT_POINT"
+
+  # Obtém o UUID do dispositivo
+  UUID=$(sudo blkid -s UUID -o value "$DEVICE")
+
+  # Adiciona uma entrada no /etc/fstab para montagem automática
+  if ! grep -q "$UUID" /etc/fstab; then
+    echo "UUID=$UUID $MOUNT_POINT xfs defaults 0 0" | sudo tee -a /etc/fstab
+  fi
+done
