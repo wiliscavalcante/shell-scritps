cb648ee02be27ad3373193e4c74577769bd26170
Wilis Cavalcante <56607831+wiliscavalcante@users.noreply.github.com>
2023-07-13 01:10:48 -0300

    Update add-volume-ec2.sh

--- add-volume-ec2.sh
+++ add-volume-ec2.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Atualiza os pacotes do sistema
+sudo yum update -y
+
 # Configuração dos dispositivos e pontos de montagem
 DEVICES=("/dev/nvme1n1" "/dev/nvme1n2")
 MOUNT_POINTS=("/data" "/backup")
@@ -9,8 +12,8 @@ for ((i=0; i<${#DEVICES[@]}; i++)); do
   DEVICE=${DEVICES[$i]}
   MOUNT_POINT=${MOUNT_POINTS[$i]}
 
-  # Verifica se o dispositivo já está formatado como XFS
-  if ! sudo xfs_info "$DEVICE" >/dev/null 2>&1; then
+  # Formata o dispositivo como XFS, se necessário
+  if ! sudo file -s "$DEVICE" | grep -q "XFS filesystem"; then
     sudo mkfs.xfs "$DEVICE"
   fi
 
@@ -29,4 +32,7 @@ for ((i=0; i<${#DEVICES[@]}; i++)); do
   if ! sudo grep -q "$UUID" /etc/fstab; then
     echo "UUID=$UUID $MOUNT_POINT xfs defaults,nofail 0 2" | sudo tee -a /etc/fstab
   fi
+
+  # Define a permissão correta para o usuário ec2-user no ponto de montagem
+  sudo chown -R ec2-user:ec2-user "$MOUNT_POINT"
 done
