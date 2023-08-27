b624b83f79f754d903c3115623ce097f934ce5ab
Wilis Cavalcante <56607831+wiliscavalcante@users.noreply.github.com>
2023-08-27 16:47:53 -0300

    Update assume-role-with-external-id.sh

--- assume-role-with-external-id.sh
+++ assume-role-with-external-id.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
 # Configurações
-ROLE_ARN="arn:aws:iam::ACCOUNT_ID:role/ROLE_NAME" # Substitua ACCOUNT_ID e ROLE_NAME
+ROLE_ARN="arn:aws:iam::ACCOUNT_ID:role/ROLE_NAME"  # Substitua ACCOUNT_ID e ROLE_NAME
 ROLE_SESSION_NAME="TemporarySession"
 TEMP_PROFILE_NAME="tempProfile"
-EXTERNAL_ID="SeuExternalID"  # Substitua com o seu valor de External ID
+EXTERNAL_ID="Your-External-ID"  # Substitua com o seu External ID real
 
-# Assume a role e guarda as credenciais temporárias em uma variável
-ASSUMED_ROLE=$(aws sts assume-role --role-arn $ROLE_ARN --role-session-name $ROLE_SESSION_NAME --external-id $EXTERNAL_ID)
+# Assume a role e guarda as credenciais temporárias em um arquivo temporário
+aws sts assume-role --role-arn $ROLE_ARN --role-session-name $ROLE_SESSION_NAME --external-id $EXTERNAL_ID > temp_creds.json
 
-# Extrai as credenciais temporárias usando 'jq'
-ACCESS_KEY=$(echo $ASSUMED_ROLE | jq -r '.Credentials.AccessKeyId')
-SECRET_ACCESS_KEY=$(echo $ASSUMED_ROLE | jq -r '.Credentials.SecretAccessKey')
-SESSION_TOKEN=$(echo $ASSUMED_ROLE | jq -r '.Credentials.SessionToken')
-EXPIRATION=$(echo $ASSUMED_ROLE | jq -r '.Credentials.Expiration')
+# Extrai as credenciais temporárias usando 'grep' e 'awk'
+ACCESS_KEY=$(grep AccessKeyId temp_creds.json | awk -F'"' '{print $4}')
+SECRET_ACCESS_KEY=$(grep SecretAccessKey temp_creds.json | awk -F'"' '{print $4}')
+SESSION_TOKEN=$(grep SessionToken temp_creds.json | awk -F'"' '{print $4}')
+EXPIRATION=$(grep Expiration temp_creds.json | awk -F'"' '{print $4}')
+
+# Remove o arquivo temporário
+rm temp_creds.json
 
 # Cria ou atualiza o perfil temporário no arquivo de configuração da AWS CLI
 aws configure set aws_access_key_id $ACCESS_KEY --profile $TEMP_PROFILE_NAME
