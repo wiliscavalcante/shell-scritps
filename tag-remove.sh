d28fae2f7c851bd64bf6f11e2c500cdc44e8d182
Wilis Cavalcante <56607831+wiliscavalcante@users.noreply.github.com>
2023-10-25 11:24:22 -0300

    Update tag-remove.sh

--- tag-remove.sh
+++ tag-remove.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
+TAG_NAME="MinhaChave"
 output_file="recursos_a_alterar.txt"
 
 # Limpar arquivo de saída
 > $output_file
 
 # Listar Auto Scaling Groups com a tag
-ASGS=$(aws autoscaling describe-auto-scaling-groups --query 'AutoScalingGroups[?contains(Tags[?Key==`MinhaChave`].Key, `MinhaChave`)].AutoScalingGroupName' --output text)
-echo "ASGs com a tag 'MinhaChave':" >> $output_file
+ASGS=$(aws autoscaling describe-auto-scaling-groups --query "AutoScalingGroups[?contains(Tags[?Key==\`${TAG_NAME}\`].Key, \`${TAG_NAME}\`)].AutoScalingGroupName" --output text)
+echo "ASGs com a tag '$TAG_NAME':" >> $output_file
 for asg in $ASGS; do
   echo $asg >> $output_file
 done
 
 # Listar instâncias EC2 com a tag
-INSTANCES=$(aws ec2 describe-instances --filters "Name=tag:MinhaChave,Values=*" --query 'Reservations[*].Instances[*].InstanceId' --output text)
-echo "\nInstâncias EC2 com a tag 'MinhaChave':" >> $output_file
+INSTANCES=$(aws ec2 describe-instances --filters "Name=tag:$TAG_NAME,Values=*" --query 'Reservations[*].Instances[*].InstanceId' --output text)
+echo "\nInstâncias EC2 com a tag '$TAG_NAME':" >> $output_file
 for instance in $INSTANCES; do
   echo $instance >> $output_file
 done
@@ -22,18 +23,19 @@ done
 cat $output_file
 
 # Pedir confirmação
-read -p "Você deseja remover a tag 'MinhaChave' dos recursos listados no arquivo '$output_file'? (s/n) " -n 1 -r
+read -p "Você deseja remover a tag '$TAG_NAME' dos recursos listados no arquivo '$output_file'? (s/n) " -n 1 -r
 echo
 if [[ $REPLY == s || $REPLY == S ]]
 then
     for asg in $ASGS; do
-      aws autoscaling delete-tags --tags "ResourceId=$asg,ResourceType=auto-scaling-group,Key=MinhaChave"
+      aws autoscaling delete-tags --tags "ResourceId=$asg,ResourceType=auto-scaling-group,Key=$TAG_NAME"
     done
 
     for instance in $INSTANCES; do
-      aws ec2 delete-tags --resources $instance --tags "Key=MinhaChave"
+      aws ec2 delete-tags --resources $instance --tags "Key=$TAG_NAME"
     done
 fi
+
 #######
 aws ec2 create-tags --resources i-1234567890abcdef0 --tags Key=NomeDaTag,Value=ValorDaTag
 aws autoscaling create-or-update-tags --tags ResourceId=my-asg-name,ResourceType=auto-scaling-group,Key=NomeDaTag,Value=ValorDaTag,PropagateAtLaunch=true
