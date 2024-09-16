e109cbf35b4b75d0537862c560a176ddf6b5e7a8
Wilis Cavalcante <56607831+wiliscavalcante@users.noreply.github.com>
2024-09-16 19:40:49 -0300

    Update teste.sh

--- teste.sh
+++ teste.sh
@@ -9,3 +9,9 @@ aws elbv2 create-target-group \
 aws elbv2 register-targets \
   --target-group-arn arn:aws:elasticloadbalancing:region:account-id:targetgroup/tg-eks-nlb-dr/xxxxxxxx \
   --targets Id=10.0.0.1,AvailabilityZone=us-east-1a Id=10.0.0.2,AvailabilityZone=us-east-1b Id=10.0.0.3,AvailabilityZone=us-east-1c
+
+aws elbv2 create-rule \
+  --listener-arn arn:aws:elasticloadbalancing:region:account-id:listener/app/my-alb/xxxxxxxx \
+  --conditions Field=path-pattern,Values='/my-service/*' \
+  --priority 10 \
+  --actions Type=forward,TargetGroupArn=arn:aws:elasticloadbalancing:region:account-id:targetgroup/tg-eks-nlb-dr/xxxxxxxx
